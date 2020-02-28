e7c212059a86dd1388ac1b1fc2d00fb1419e19ee
Carlos Santana <csantana@us.ibm.com>
2020-02-28 13:54:22 -0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Cloud Native !' });
+  res.render('index', { title: 'Cloud Native demo Feb 28th!' });
 });
 
 module.exports = router;
