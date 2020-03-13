6bfefa28ef442be99bf96613a5067d656e22071b
Carlos Santana <csantana23@gmail.com>
2020-03-12 22:05:33 -0400

    demo prep march 12

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Cloud Native demo Feb 28th!' });
+  res.render('index', { title: 'Cloud Native demo March 12th 2020' });
 });
 
 module.exports = router;
